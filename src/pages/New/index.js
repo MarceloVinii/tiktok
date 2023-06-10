c626e835ef0f0f4c6600601e772cf7953cce8ec4
Marcelo Vinícius <marcelo.vaz231@gmail.com>
2023-06-10 10:28:25 -0300

    iniciando e configurando navegação e rotas

---
+++ src/pages/New/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+import styles from "./styles"
+
+export default function New() {
+  return (
+    <View style={styles.container}>
+      <Text>Pagina New</Text>
+    </View>
+  )
+}
